708ff286852c3cf7697fec776d643a210bb8cf90
SnowyCommander <gouahra@gmail.com>
2024-08-21 13:03:11 +0900

    UI 구현 중. viewport width안에서만 flexible하게 아이템 모양이 결정되게 하는 레이아웃을 구현하는 게 힘들다.

--- script1.js
+++ script1.js
@@ -4,11 +4,13 @@ $(document).ready(function () {
   async function getData() {
     try {
       const response = await fetch(
-        "https://api.odcloud.kr/api/15127329/v1/uddi:293873a5-92dc-46a4-b534-8c12bea5b2de?serviceKey=rF1zZu77tv0ZegJsuAuoxJUC367OaJgWMauVE6Vxjq8ccKxJE8chA5Mja++MGvtEG9nmEAIXpK/03iKD0b+X6A==&page=1&perPage=10"
+        "https://api.odcloud.kr/api/15127329/v1/uddi:293873a5-92dc-46a4-b534-8c12bea5b2de?page=1&perPage=10&serviceKey=rF1zZu77tv0ZegJsuAuoxJUC367OaJgWMauVE6Vxjq8ccKxJE8chA5Mja%2B%2BMGvtEG9nmEAIXpK%2F03iKD0b%2BX6A%3D%3D"
       );
       if (response.ok) {
         const data = await response.json();
         console.log(data);
+      } else if (response.status === 400) {
+        throw new Error("등록되지 않은 인증키 입니다.");
       } else if (response.status === 401) {
         throw new Error("인증 정보가 정확 하지 않음");
       } else if (response.status === 404) {
@@ -24,4 +26,6 @@ $(document).ready(function () {
   }
 
   getData();
+
+  
 });
