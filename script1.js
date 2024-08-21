f0d7bcc8f61aed0a3bf384377ad4168aa8e8748d
SnowyCommander <gouahra@gmail.com>
2024-08-21 14:23:22 +0900

    PPT발표 전 급한대로 기능구현한 것

--- script1.js
+++ script1.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-  const allPosts = $("#allPosts");
-  let posts = [];
   async function getData() {
     try {
       const response = await fetch(
@@ -9,6 +7,7 @@ $(document).ready(function () {
       if (response.ok) {
         const data = await response.json();
         console.log(data);
+        return data;
       } else if (response.status === 400) {
         throw new Error("등록되지 않은 인증키 입니다.");
       } else if (response.status === 401) {
@@ -25,7 +24,21 @@ $(document).ready(function () {
     }
   }
 
-  getData();
+  const allPosts = $("#allPosts");
+  let posts = [];
 
-  
-});
+  getData().then((data)=>{data.data.forEach((post)=>{
+    // // 빈 항목 생성 후 값 기입하여 생성한 포스트를 ul에 붙이기
+        const newFramePost = $('<li class="post"></li>');
+        newFramePost.append(`<div class="Dong">${post["행정동"]}</div>`)
+                    .append(`<div class="address">${post["상세주소"]}"</div>`)
+                    .append(`<div class="owner">${post["소유자"]}</div>`)
+                    .append(`<div class="area">userId : ${post["면적"]}</div>`)
+                    .append(`<div class="dateOfconfirmEmpty">${post["빈집판정일"]}</div>`)
+                    .append(`<div class="dateOfdataCreated">${post["데이터기준일자"]}</div>`)
+                    .append(`<div class="grade">${post["등급"]}</div>`)
+                    .append(`<div class="index">${post["연번"]}</div>`);
+        $('#allPosts').append(newFramePost);
+     })
+    })
+  });
